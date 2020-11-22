264f83d60f8cb4d95c6f28a39c601154411c5e2f
Kzim <kazemaraby@gmail.com>
2020-11-22 15:32:19 +0200

    departmen role

--- erpnext/hr/doctype/mission/mission.js
+++ erpnext/hr/doctype/mission/mission.js
@@ -2,7 +2,15 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Mission', {
-	// refresh: function(frm) {
 
-	// }
+	end_time:function(frm){
+		var sDate=(frm.doc.start_time).split(':')
+        var tDate=(frm.doc.end_time).split(':')
+        var difInHourse=tDate[0]-sDate[0]
+        var difInMinutes=tDate[1]-sDate[1]
+        difInMinutes+=difInHourse*60;
+        console.log(difInMinutes);
+		frm.set_value('duration', difInMinutes);
+	}
+
 });
