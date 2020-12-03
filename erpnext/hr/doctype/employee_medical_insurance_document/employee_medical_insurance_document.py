90e3bb6b8fb1c957729697ebcf07445500ca8747
beshoy <beshoyatef31@gmail.com>
2020-12-03 17:00:48 +0200

    add health insurance

--- erpnext/hr/doctype/employee_medical_insurance_document/employee_medical_insurance_document.py
+++ erpnext/hr/doctype/employee_medical_insurance_document/employee_medical_insurance_document.py
@@ -3,8 +3,17 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class EmployeeMedicalInsuranceDocument(Document):
-	pass
+	def on_submit(self):
+		a= frappe.db.sql(""" UPDATE `tabEmployee` SET document = '%s' , medication_card_recieving_date ='%s',employee_share_ratio ='%s'
+		 WHERE name='%s' """%(self.insurance_document ,self.insurance_card_start_date , self.employee_fee ,self.employee ))
+
+
+@frappe.whitelist()
+def get_employee(*args,**kwargs):
+
+	employees = frappe.db.sql(""" SELECT name FROM `tabEmployee` WHERE status="Active"  AND document IS NULL   """)
+	return(employees)
