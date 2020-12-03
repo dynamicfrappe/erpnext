a8699b9a28b8a1f9aaf19d9db133e5de33f6b553
beshoy <beshoyatef31@gmail.com>
2020-12-03 15:16:25 +0200

    add

--- erpnext/hr/doctype/employee_medical_insurance_document/employee_medical_insurance_document.js
+++ erpnext/hr/doctype/employee_medical_insurance_document/employee_medical_insurance_document.js
@@ -5,4 +5,24 @@ frappe.ui.form.on('Employee Medical Insurance Document', {
 	// refresh: function(frm) {
 
 	// }
+	refresh :function(frm){
+		if (frm.doc.include_family_members == '1' && frm.is_new()){
+		
+				frm.set_df_property('employee_medical_insurance_members' , 'hidden' ,1);
+		}else{
+			frm.set_df_property('employee_medical_insurance_members' , 'hidden' ,0);
+		}
+
+	},
+	include_family_members:function(frm){
+		if (frm.doc.include_family_members == '1'){
+			frm.set_df_property('employee_medical_insurance_members' , 'hidden' ,0);
+			frm.refresh_field('employee_medical_insurance_members')
+		}
+		else{
+			frm.clear_table('employee_medical_insurance_members')
+			frm.set_df_property('employee_medical_insurance_members' , 'hidden' ,1);
+			frm.refresh_field('employee_medical_insurance_members')
+		}
+	}
 });
