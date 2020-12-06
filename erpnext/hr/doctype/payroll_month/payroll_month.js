7f85f9c50561b0a5d504a7197e724fbfc37a0a76
beshoy <beshoyatef31@gmail.com>
2020-12-06 14:11:54 +0200

    add

--- erpnext/hr/doctype/payroll_month/payroll_month.js
+++ erpnext/hr/doctype/payroll_month/payroll_month.js
@@ -5,18 +5,72 @@ frappe.ui.form.on('Payroll Month', {
 	// refresh: function(frm) {
 
 	// }
-    setup: function(frm) {
-		frm.trigger("get_years");
-	 },
-	 get_years: function(frm) {
-		return  frappe.call({
+	onload:function(frm){
+		frappe.call({
 			method: "get_attendance_years",
 			doc : frm.doc,
 			callback: function(r) {
-				debugger;
 				frm.set_df_property("year", "options", r.message );
 				refresh_field("year");
 			}
 		});
+
+		
+
+	},
+
+	refresh: function(frm) {
+	if (frm.doc.docstatus == 1){
+			
+				frm.add_custom_button(__("Create Payroll"), function() {
+					frm.events.create_payroll(frm)
+						
+					}).addClass("btn-primary");
+		}
+	},
+	create_payroll:function(frm){
+		var dialog = new frappe.ui.Dialog({
+			title: __("Payroll Type "),
+			fields: [
+				{	"fieldtype": "Select", "label": __("Payroll"),
+					"fieldname": "payroll_type",
+					"options": ["A" , "B"],
+					"reqd": 1,
+					 },
+				{	"fieldtype": "Button", "label": __('Create Payroll Entrs'),
+					"fieldname": "create_payroll_entry", "cssClass": "btn-primary" },
+			]
+		});
+		dialog.fields_dict.create_payroll_entry.$input.click(function() {
+			var args = dialog.get_values();
+			console.log(args)
+			dialog.hide();
+		})
+		dialog.show()
+
+	},
+	month:function(frm){
+		frappe.call({
+			doc:frm.doc ,
+			"method":"month_for_year" ,
+			callback:function(r){
+				if (!r.message){
+					var mo = frm.doc.month
+					frm.set_value("month" ," ")
+					frappe.throw((" year " + frm.doc.year + "Already Have Month " + mo ))
+				}
+			}
+		})
+
+	},
+	start_date:function(frm){
+		frappe.call({
+			doc:frm.doc ,
+			"method":"set_end_date" ,
+			callback:function(r){
+				frm.set_value("end_date" ,r.message)
+				frm.refresh_field("end_date")
+			}
+		})
 	}
 });
--- erpnext/hr/doctype/payroll_month/payroll_month.py
+++ erpnext/hr/doctype/payroll_month/payroll_month.py
@@ -3,11 +3,27 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 from datetime import  datetime
+
+
+from dateutil.relativedelta import relativedelta
 class PayrollMonth(Document):
+	def validate(self):
+		if not self.month_for_year():
+			frappe.throw("Month validation Error")
+		if datetime.strptime(self.end_date, "%Y-%m-%d").date() != self.set_end_date():
+			frappe.throw("Invalid End Date")
 	def get_attendance_years(self):
 		year = datetime.today().year
-		year_list = range(2019 ,year+1 )
+		year_list =[ x for x in range(2019 ,year+1 )]
 		return "\n".join(str(x) for x in year_list)
+	def month_for_year(self):
+		dublicated = frappe.db.sql("""SELECT name FROM `tabPayroll Month` WHERE month='%s' AND year='%s' and docstatus=1"""%(self.month ,self.year))
+		if dublicated:
+			return False
+		else:
+			return True
+	def set_end_date(self):
+		return  (datetime.strptime(self.start_date, "%Y-%m-%d").date() +  relativedelta(days=-1)+ relativedelta(months=+1))
