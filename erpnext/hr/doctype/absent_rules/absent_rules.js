21671b01407954d284cef68842a92f17cc8eb8fc
kzim <kazemaraby@gmail.com>
2020-12-07 16:32:57 +0200

    Added Value Tax

--- erpnext/hr/doctype/absent_rules/absent_rules.js
+++ erpnext/hr/doctype/absent_rules/absent_rules.js
@@ -2,7 +2,46 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Absent Rules', {
-	// refresh: function(frm) {
+	 refresh: function(frm) {
 
-	// }
+	 },
+    sort_table : function (frm, cdt, cdn){
+
+	     var list = ['First Day','Second Day','Third Day','Fourth Day','Fifth Day'] ;
+	     var i = 0 ;
+	     frm.doc.ruletemplate.forEach((e)=>{
+	         e.rule = list [i++];
+	         if (i >=list.length)
+             {
+                  frm.get_field('ruletemplate').grid.cannot_add_rows = true;
+                  refresh_field("ruletemplate");
+                  return;
+             }
+         });
+        if (i >= list.length)
+        {
+              frm.get_field('ruletemplate').grid.cannot_add_rows = true;
+        }
+        else {
+             frm.get_field('ruletemplate').grid.cannot_add_rows = false;
+        }
+        refresh_field("ruletemplate");
+        }
+});
+
+frappe.ui.form.on('absent rule templates', {
+        rule: function (frm, cdt, cdn) {
+            frm.events.sort_table(frm, cdt, cdn);
+
+
+
+        },
+
+        ruletemplate_add: function (frm, cdt, cdn) {
+             frm.events.sort_table(frm, cdt, cdn);
+        },
+        ruletemplate_remove: function (frm, cdt, cdn) {
+             frm.events.sort_table(frm, cdt, cdn);
+        }
+
 });
