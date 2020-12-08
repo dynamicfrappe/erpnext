fe89618a15eb175d137b1676031f08dc870272af
beshoy <beshoyatef31@gmail.com>
2020-12-08 14:58:03 +0200

    add

--- erpnext/hr/doctype/monthly_salary_slip/monthly_salary_slip.py
+++ erpnext/hr/doctype/monthly_salary_slip/monthly_salary_slip.py
@@ -1,10 +1,70 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
-# For license information, please see license.txt
-
 from __future__ import unicode_literals
-# import frappe
-from frappe.model.document import Document
+import frappe, erpnext
+import datetime, math
 
-class MonthlySalarySlip(Document):
-	pass
+from frappe.utils import add_days, cint, cstr, flt, getdate, rounded, date_diff, money_in_words
+from frappe.model.naming import make_autoname
+
+from frappe import msgprint, _
+from erpnext.hr.doctype.payroll_entry.payroll_entry import get_start_end_dates
+from erpnext.hr.doctype.employee.employee import get_holiday_list_for_employee
+from erpnext.utilities.transaction_base import TransactionBase
+from frappe.utils.background_jobs import enqueue
+from erpnext.hr.doctype.additional_salary.additional_salary import get_additional_salary_component
+from erpnext.hr.doctype.payroll_period.payroll_period import get_period_factor, get_payroll_period
+from erpnext.hr.doctype.employee_benefit_application.employee_benefit_application import get_benefit_component_amount
+from erpnext.hr.doctype.employee_benefit_claim.employee_benefit_claim import get_benefit_claim_amount, get_last_payroll_period_benefits
+
+
+class MonthlySalarySlip(TransactionBase):
+	def __init__(self, *args, **kwargs):
+		super(MonthlySalarySlip, self).__init__(*args, **kwargs)
+		self.series = 'Sal Slip/{0}/.#####'.format(self.employee)
+		self.whitelisted_globals = {
+			"int": int,
+			"float": float,
+			"long": int,
+			"round": round,
+			"date": datetime.date,
+			"getdate": getdate
+		}
+		self.total_working_days_temp = 0
+		self.daily_rate = 0
+
+	def autoname(self):
+		self.name = make_autoname(self.series)
+
+
+	def validate(self):
+		self.status = self.get_status()
+		self.validate_dates()
+		self.check_existing()
+
+
+	def validate_dates(self):
+		if date_diff(self.end_date, self.start_date) < 0:
+			frappe.throw(_("To date cannot be before From date"))
+
+	def get_status(self):
+		if self.docstatus == 0:
+			status = "Draft"
+		elif self.docstatus == 1:
+			status = "Submitted"
+		elif self.docstatus == 2:
+			status = "Cancelled"
+		return status
+
+
+	def check_existing(self):
+		if not self.salary_slip_based_on_timesheet:
+			ret_exist = frappe.db.sql("""select name from `Monthly Salary Slip`
+						where start_date = %s and end_date = %s and docstatus != 2
+						and employee = %s and name != %s and payroll_type='%s""",
+						(self.start_date, self.end_date, self.employee, self.name ,self.payroll_type))
+			if ret_exist:
+				self.employee = ''
+				frappe.throw(_("Salary Slip of employee {0} already created for this period").format(self.employee))
+		else:
+			for data in self.timesheets:
+				if frappe.db.get_value('Timesheet', data.time_sheet, 'status') == 'Payrolled':
+					frappe.throw(_("Salary Slip of employee {0} already created for time sheet {1}").format(self.employee, data.time_sheet))
