b368a19d6c8dfb753dc954b3aa7605716e33c2a5
Kzim <kazemaraby@gmail.com>
2020-12-10 16:52:49 +0200

    asd

--- erpnext/hr/doctype/request_for_expence_claim/request_for_expence_claim.js
+++ erpnext/hr/doctype/request_for_expence_claim/request_for_expence_claim.js
@@ -2,7 +2,19 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Request for expence claim', {
-    // refresh: function(frm) {
-
-    // }
+    setup: function(frm) {
+		frappe.call({
+            method:"getEmployee",
+            doc:frm.doc,
+            callback(r){
+                frm.set_query("employee", function() {
+			return {
+				filters: [
+					["Employee","name", "in", r.message]
+				]
+			}
+		});
+            }
+        })
+	}
 });
--- erpnext/hr/doctype/request_for_expence_claim/request_for_expence_claim.py
+++ erpnext/hr/doctype/request_for_expence_claim/request_for_expence_claim.py
@@ -3,8 +3,21 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Requestforexpenceclaim(Document):
-	pass
+	def validate(self):
+		total=0
+		for ex in self.expence:
+			total +=int(ex.amount)
+		self.total=total
+
+	def getEmployee(self):
+		emplist = []
+		data = frappe.db.sql("""select employee from `tabEmployee Advance`""", as_dict=1)
+		for emp in data:
+			emplist.append(emp.employee)
+		return emplist
+
+
