99d854d606af940e0e8281e1173852f45f88cec9
Kzim <kazemaraby@gmail.com>
2020-12-13 12:13:08 +0200

    asd

--- erpnext/utills.py
+++ erpnext/utills.py
@@ -1,5 +1,5 @@
 import frappe
 import datetime
 def AdditionalSalaries(employee,month):
-    result=frappe.db.sql("select type,sum(amount) as 'Amount' from `tabAdditional Salary` where employee='{}' and month(payroll_date)='{}' group by type".format(employee,month),as_dict=1)
+    result=frappe.db.sql("select type,amount as 'Amount' from `tabAdditional Salary` where employee='{}' and month(payroll_date)='{}'".format(employee,month),as_dict=1)
     return result
