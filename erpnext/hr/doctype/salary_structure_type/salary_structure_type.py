f09e702cdf92b0ae5e5b630b0c0c490a772924f9
kzim <kazemaraby@gmail.com>
2020-12-14 15:21:18 +0200

    monthly salary slip

--- erpnext/hr/doctype/salary_structure_type/salary_structure_type.py
+++ erpnext/hr/doctype/salary_structure_type/salary_structure_type.py
@@ -3,18 +3,19 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class SalaryStructureType(Document):
 	def validate(self):
 		if self.is_main:
 			frappe.db.sql("""
-			update `TabSalary Structure Type`  set is_main = 0 where is_main = 1
+			update `tabSalary Structure Type`  set is_main = 0 where is_main = 1
 			""")
 		else:
 			result = frappe.db.sql("""
-			select * from `TabSalary Structure Type`   where is_main = 1
+			select * from `tabSalary Structure Type`   where is_main = 1
 			""")
 			if not result :
 				self.is_main = 1
