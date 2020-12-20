1db510d8c7c5cae58afad98001852c342389ebe7
kzim <kazemaraby@gmail.com>
2020-12-20 14:10:27 +0200

    Payroll Month

--- erpnext/hr/doctype/payroll_month/payroll_month.py
+++ erpnext/hr/doctype/payroll_month/payroll_month.py
@@ -4,12 +4,30 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe import _
 from frappe.model.document import Document
-from datetime import  datetime
+from datetime import  datetime ,timedelta
+import calendar
 
 
 from dateutil.relativedelta import relativedelta
 class PayrollMonth(Document):
+	def cancel(self):
+		frappe.throw(_("Payroll Month is n't be able to cancel"))
+	def submit(self):
+		last_doc = frappe.db.sql("""
+				 select * from `tabPayroll Month`  where company = '{company}' and docstatus = 1 and ifnull(is_closed,0) = 0  order by start_date desc LIMIT 1
+							""".format(company=self.company), as_dict=1)[0]
+		if last_doc :
+			frappe.throw(_("Please Close the opened Month {} first").format(last_doc.name))
+		self.is_closed = 0
+	def close_month(self):
+		salary_slip_list = frappe.db.sql("""select * from `tabMonthly Salary Slip` where  month = '{name}' and docstatus = 1""".format(name=self.name))
+		if not salary_slip_list :
+			frappe.throw(_('There is  No Submitted Salary Slip in this Month'))
+		frappe.db.sql("""update `tabPayroll Month` set is_closed = 1  where name = '{name}' """.format(name=self.name))
+		self.is_closed = 1
+		frappe.msgprint(_("Done") ,title=_("Success"), indicator="green")
 	def validate(self):
 		if not self.month_for_year():
 			frappe.throw("Month validation Error")
@@ -17,8 +35,8 @@ class PayrollMonth(Document):
 			frappe.throw("Invalid End Date")
 	def get_attendance_years(self):
 		year = datetime.today().year
-		year_list = range(2019 ,year+2 )
-
+		year_list = [ i for i in range(2019 ,year+5 )]
+		year_list.reverse()
 		return "\n".join(str(x) for x in year_list)
 	def month_for_year(self):
 		dublicated = frappe.db.sql("""SELECT name FROM `tabPayroll Month` WHERE month='%s' AND year='%s' and docstatus=1"""%(self.month ,self.year))
@@ -35,6 +53,37 @@ class PayrollMonth(Document):
 	def get_strcuture_type_option(self):
 		a=  frappe.db.sql(""" SELECT type FROM `tabSalary Structure Type` """)
 		return([i[0] for i in a])
+	def set_start_date (self):
+		start_date = None
+		attendance_start_date = None
+		last_doc = frappe.db.sql("""
+		 select * from `tabPayroll Month`  where company = '{company}'  order by start_date desc LIMIT 1
+					""".format(company=self.company),as_dict=1)
+
+		if not last_doc:
+
+			start_day = frappe.db.get_single_value("HR Settings","payroll_month_start_day") or 0
+			if not start_day:
+				frappe.throw(_("Please Set Payroll Month Start Day in HR Settings"))
+			attendance_start_day = frappe.db.get_single_value("HR Settings","attendance_month_start_day") or start_day
+			month = datetime.strptime(str(self.month),  "%B").month
+			self.start_date = datetime(int(self.year), month, start_day).date()
+			self.attendance_start_date = datetime(int(self.year), month, attendance_start_day).date()
+			self.end_date = datetime.strptime(str(self.start_date), "%Y-%m-%d").date() + relativedelta(
+				months=+1) + relativedelta(days=-1)
+			self.attendance_end_date = datetime.strptime(str(self.attendance_start_date),
+														 "%Y-%m-%d").date() + relativedelta(months=+1) + relativedelta(
+				days=-1)
+
+		else:
+			last_doc = last_doc[0]
+			self.start_date = datetime.strptime(str(last_doc.start_date), "%Y-%m-%d").date() +  relativedelta(months=+1)
+			self.attendance_start_date = datetime.strptime(str(last_doc.attendance_start_date), "%Y-%m-%d").date() +  relativedelta(months=+1)
+
+			self.end_date = datetime.strptime(str(self.start_date), "%Y-%m-%d").date() + relativedelta(months=+1) + relativedelta(days=-1)
+			self.attendance_end_date = datetime.strptime(str(self.attendance_start_date),"%Y-%m-%d").date() + relativedelta(months=+1) + relativedelta(days=-1)
+			self.month = self.end_date.strftime("%B")
+			self.year = str(self.end_date.strftime("%Y"))
 
 
 @frappe.whitelist()
