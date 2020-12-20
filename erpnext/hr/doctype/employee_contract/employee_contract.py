7500b80b8874af81f301df38ff623f2d099d167d
Kzim <kazemaraby@gmail.com>
2020-12-20 14:43:21 +0200

    emp conract

--- erpnext/hr/doctype/employee_contract/employee_contract.py
+++ erpnext/hr/doctype/employee_contract/employee_contract.py
@@ -3,8 +3,7 @@
 # For license information, please see license.txt
 from __future__ import unicode_literals
 from frappe import utils
-import datetime 
-
+import datetime
 from dateutil.relativedelta import relativedelta
 
 import frappe
@@ -36,7 +35,7 @@ class EmployeeContract(Document):
 		self.check_ended_contract()
 
 	def check_ended_contract(self):
-		is_ended = datetime.datetime.strptime(self.contract_end_date, '%Y-%m-%d').date() > datetime.datetime.strptime(utils.today(), '%Y-%m-%d').date()
+		is_ended = datetime.datetime.strptime(str(self.contract_end_date), '%Y-%m-%d').date() < datetime.datetime.strptime(str(utils.today()), '%Y-%m-%d').date()
 		if is_ended :
 				self.status = "Ended"
 
@@ -46,7 +45,7 @@ class EmployeeContract(Document):
 		if frappe.db.exists("Employee", self.employee):
 
 			employee_active_contract = frappe.db.sql("""SELECT contract_end_date FROM `tabEmployee Contract` WHERE 
-				 employee ='%s'"""%self.employee)
+				 employee ='%s' and status='Active'"""%self.employee)
 			if employee_active_contract:
 				end_date =employee_active_contract[0][0]
 		if end_date:
@@ -54,6 +53,35 @@ class EmployeeContract(Document):
 			if not valid_start_date:
 				return end_date+relativedelta(days=+1)
 		return False
+
+	def RenewContract(self):
+		end_date=datetime.datetime.strptime(self.contract_end_date, '%Y-%m-%d').date()
+		doc = frappe.new_doc("Employee Contract")
+		doc.employee=self.employee
+		doc.employee_name=self.employee_name
+		doc.national_id=self.national_id
+		doc.employee_addresse=self.employee_addresse
+		doc.employee_joined_day=self.employee_joined_day
+		doc.compnay=self.compnay
+		doc.contract_type=self.contract_type
+		doc.company_representative_name=self.company_representative_name
+		doc.status="Active"
+		doc.company_representative_jop_title=self.company_representative_jop_title
+		doc.company_addresse=self.company_addresse
+		doc.contract_duration=self.contract_duration
+		doc.contract_start_date=end_date+relativedelta(days=+1)
+		doc.contract_end_date=end_date+relativedelta(months=+int(self.contract_duration))
+		doc.department=self.department
+		doc.branch=self.branch
+		doc.designation=self.designation
+		doc.comprehensive_salary=self.comprehensive_salary
+		doc.insurance_salary=self.insurance_salary
+		doc.net_salary=self.net_salary
+		doc.contract_template=self.contract_template
+		doc.attached_image=self.attached_image
+		doc.save()
+		#frappe.set_route("Form", "Employee Contract", doc.name);
+
 
 
 @frappe.whitelist()
