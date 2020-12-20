7495c1aa2cc7136d896f076f991ab74d8b28fc1e
beshoy <beshoyatef31@gmail.com>
2020-12-20 13:50:31 +0200

    updatecontract

--- erpnext/hr/doctype/employee_contract/employee_contract.py
+++ erpnext/hr/doctype/employee_contract/employee_contract.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 from __future__ import unicode_literals
-
+from frappe import utils
 import datetime 
 
 from dateutil.relativedelta import relativedelta
@@ -23,13 +23,25 @@ class EmployeeContract(Document):
 			if self.designation:
 				frappe.db.sql("update tabEmployee set designation='{}' where name='{}'".format(self.designation, self.employee))
 	
+		if self.testing_duration and self.testing_start_date and self.testing_end_date :
+			in_testing =  datetime.datetime.strptime(self.testing_end_date, '%Y-%m-%d').date() > datetime.datetime.strptime(utils.today(), '%Y-%m-%d').date()
+			if in_testing :
+				self.status = "On testing period"
+			else:
+				self.status = "Active"
 
+		if self.testing_duration == 0 :
+			self.status = "Active"
 
+		self.check_ended_contract()
 
-
-
+	def check_ended_contract(self):
+		is_ended = datetime.datetime.strptime(self.contract_end_date, '%Y-%m-%d').date() > datetime.datetime.strptime(utils.today(), '%Y-%m-%d').date()
+		if is_ended :
+				self.status = "Ended"
 
 	def validate_employee_active_cotracts(self):
+		# frappe.throw( str(utils.today()))
 		end_date= False
 		if frappe.db.exists("Employee", self.employee):
 
