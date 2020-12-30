380736eafcf2098f3a227d87b178fd134eb20a90
Kzim <kazemaraby@gmail.com>
2020-12-30 11:57:09 +0200

    asd

--- erpnext/hr/doctype/vehicle/vehicle.js
+++ erpnext/hr/doctype/vehicle/vehicle.js
@@ -10,6 +10,59 @@ frappe.ui.form.on('Vehicle', {
 		  for (var i = max; i >= min; i--) {
 			years.push(i)
 		  }
-		  
+			frm.set_df_property('model', 'options', years);
 	}
+});
+  frappe.ui.form.on("Licence Grid", "active", function (frm, cdt, cdn) {
+  	var child = locals[cdt][cdn]
+  		var count =0;
+  	  for (var c=0; c< frm.doc.licences.length;c++){
+  	  	// console.log(frm.doc.licences[c])
+  	  	 if(frm.doc.licences[c].active==1){
+  	  	 	count ++
+
+		 }
+  	  	 if (count>1){
+
+  	  	 	child.active=0;
+  	  	 	refresh_field("licences");
+  	  	 	frappe.throw("You Cant Select two licence");
+		 }
+	  }
+
+	  frappe.call({
+		  method: 'checkLicenceValidation',
+		  doc: frm.doc,
+		  args:{
+		  	"endDtae":child.end_date
+		  },
+		  callback(r) {
+
+
+				if(r.message=='false'){
+					child.active=0;
+					frappe.msgprint("This Licence is expired")
+					 refresh_field("licences");
+
+				}
+		  }
+
+	  });
+  })
+
+frappe.ui.form.on("Licence Grid", "licence", function(frm,cdt,cdn){
+       var child = locals[cdt][cdn]
+         var count=0
+		 for (var c=0; c< frm.doc.licences.length;c++){
+
+  	  	 if (child.licence==frm.doc.licences[c].licence ){
+			count++
+			 if (count>1) {
+				 child.licence = "";
+				 refresh_field("licences");
+				 frappe.throw("Licence Already Exist");
+			 }
+		 }
+	  }
+
 });
--- erpnext/hr/doctype/vehicle/vehicle.py
+++ erpnext/hr/doctype/vehicle/vehicle.py
@@ -7,10 +7,18 @@ import frappe
 from frappe import _
 from frappe.utils import getdate
 from frappe.model.document import Document
-
+from frappe.utils import getdate, validate_email_address, today, add_years, format_datetime, cstr
+import datetime
+from dateutil.relativedelta import relativedelta
 class Vehicle(Document):
 	def validate(self):
-		if getdate(self.start_date) > getdate(self.end_date):
-			frappe.throw(_("Insurance Start date should be less than Insurance End date"))
-		if getdate(self.carbon_check_date) > getdate():
-			frappe.throw(_("Last carbon check date cannot be a future date"))
+		pass
+		# if getdate(self.start_date) > getdate(self.end_date):
+		# 	frappe.throw(_("Insurance Start date should be less than Insurance End date"))
+		# if getdate(self.carbon_check_date) > getdate():
+		# 	frappe.throw(_("Last carbon check date cannot be a future date"))
+
+	def checkLicenceValidation(self,endDtae):
+		if getdate(endDtae) > getdate(today()):
+			return 'true'
+		return 'false'
