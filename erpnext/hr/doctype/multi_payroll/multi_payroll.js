ff79dc9dc73698468a7a374538966333556f002a
EngPeterMaged <eng.peter.maged@gmail.com>
2021-01-05 17:13:30 +0200

    payroll

--- erpnext/hr/doctype/multi_payroll/multi_payroll.js
+++ erpnext/hr/doctype/multi_payroll/multi_payroll.js
@@ -72,22 +72,45 @@ frappe.ui.form.on('Multi Payroll', {
 
 			}
 		}
+		if (frm.doc.docstatus == 1) {
+					if (frm.custom_buttons) frm.clear_custom_buttons();
+					frm.events.add_context_buttons(frm);
+				}
 
 
 
-		if(!frm.is_new() && frm.doc.salary_slips_created == 1) {
 
-			frm.page.clear_primary_action();
+	},
+	add_context_buttons: function(frm) {
+    	debugger;
+		if(frm.doc.salary_slips_submitted || (frm.doc.__onload && frm.doc.__onload.submitted_ss)) {
+			frm.events.add_bank_entry_button(frm);
+		} else if(frm.doc.salary_slips_created) {
+			frm.add_custom_button(__("Submit Salary Slip"), function() {
+				submit_salary_slip(frm);
+			}).addClass("btn-primary");
 			frm.add_custom_button(__("Calculate"),
 				function() {
 					frm.events.calculate_salary_slip(frm);
 				}
 			).toggleClass('btn-primary', !(frm.doc.employees || []).length);
-
-
 		}
+	},
 
-
+	add_bank_entry_button: function(frm) {
+		frappe.call({
+			method: 'erpnext.hr.doctype.multi_payroll.multi_payroll.payroll_entry_has_bank_entries',
+			args: {
+				'name': frm.doc.name
+			},
+			callback: function(r) {
+				if (r.message && !r.message.submitted) {
+					frm.add_custom_button("Make Bank Entry", function() {
+						make_bank_entry(frm);
+					}).addClass("btn-primary");
+				}
+			}
+		});
 	},
 	calculate_salary_slip : function (frm){
 		return frappe.call({
@@ -137,4 +160,54 @@ frappe.ui.form.on('Multi Payroll', {
 			}
 		})
 	},
+
 });
+const submit_salary_slip = function (frm) {
+	frappe.confirm(__('This will submit Salary Slips and create accrual Journal Entry. Do you want to proceed?'),
+		function() {
+			frappe.call({
+				method: 'submit_salary_slips',
+				args: {},
+				callback: function() {frm.events.refresh(frm);},
+				doc: frm.doc,
+				freeze: true,
+				freeze_message: __('Submitting Salary Slips and creating Journal Entry...')
+			});
+		},
+		function() {
+			if(frappe.dom.freeze_count) {
+				frappe.dom.unfreeze();
+				frm.events.refresh(frm);
+			}
+		}
+	);
+};
+
+let make_bank_entry = function (frm) {
+	var doc = frm.doc;
+	if (doc.payment_account) {
+		return frappe.call({
+			doc: cur_frm.doc,
+			method: "make_payment_entry",
+			callback: function(r) {
+				debugger;
+				frappe.set_route(
+					'List', 'Journal Entry', {"Journal Entry Account.reference_name": frm.doc.name}
+				);
+			},
+			freeze: true,
+			freeze_message: __("Creating Payment Entries......")
+		});
+	} else {
+		frappe.msgprint(__("Payment Account is mandatory"));
+		frm.scroll_to_field('payment_account');
+	}
+};
+
+let render_employee_attendance = function(frm, data) {
+	frm.fields_dict.attendance_detail_html.html(
+		frappe.render_template('employees_to_mark_attendance', {
+			data: data
+		})
+	);
+}
