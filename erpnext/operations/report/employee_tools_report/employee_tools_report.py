29220f6d56223b5ddcf66cd1befd3b55f20e3205
Kzim <kazemaraby@gmail.com>
2021-04-27 19:03:03 +0200

    employee tools report

--- erpnext/operations/report/employee_tools_report/employee_tools_report.py
+++ erpnext/operations/report/employee_tools_report/employee_tools_report.py
@@ -35,18 +35,32 @@ def get_columns(filters):
             "options":"Customer Agreement",
             "width": 150
         },
+
+        {
+            "label": _("Customer"),
+            "fieldname": "customer",
+            "fieldtype": "Data",
+            "width": 150
+        },
+        {
+            "label": _("Item"),
+            "fieldname": "item",
+            "fieldtype": "Link",
+            "options":"Item",
+            "width": 150
+        },
+        {
+            "label": _("QTY"),
+            "fieldname": "qty",
+            "fieldtype": "Data",
+            "width": 150
+        },
         {
             "label": _("Item Status"),
             "fieldname": "item_status",
             "fieldtype": "Data",
             "width": 150
         },
-        {
-            "label": _("Customer"),
-            "fieldname": "customer",
-            "fieldtype": "Data",
-            "width": 150
-        }
 
     ]
 
@@ -58,19 +72,21 @@ def get_data(filters):
     if filters.get("employee"):
         condition += " and employee='%s'" % filters.get("employee")
     if filters.get("customer_agreement"):
-        condition += " and customer_agreement='%s'"%filters.get("customer_agreement")
+        condition += " and child.customer_agrement='%s'"%filters.get("customer_agreement")
     if filters.get("item_status"):
-        condition += " and item_status='%s'"%filters.get("item_status")
+        condition += " and child.status='%s'"%filters.get("item_status")
     if filters.get("customer"):
-        condition += " and `tabCustomer Agrement`.customer='%s'"%filters.get("customer")
+        condition += " and child.customer='%s'"%filters.get("customer")
 
     results = frappe.db.sql("""  
-               select employee,employee_name,customer_agreement,item_status,`tabCustomer Agrement`.customer as 'customer'
-            from
-                 `tabEmployee Tools`
-            inner join `tabCustomer Agrement`
-            on `tabCustomer Agrement`.name=customer_agreement
-			{condition}
+               select employee,employee_name,customer_agreement,child.item_code as 'item',child.status as 'item_status',
+               child.qty as 'qty',child.customer as 'customer',child.customer_agrement as 'customer_agrement'
+                from `tabEmployee Tools`
+                inner join `tabEmployee Tools Item` child
+                on child.parent=`tabEmployee Tools`.name
+                {condition}
+                group by employee, employee_name, customer_agreement, child.item_code, child.status, child.qty, child.customer
+			
 	""".format(condition=condition), as_dict=1)
 
     return results
