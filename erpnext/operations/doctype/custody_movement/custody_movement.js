15c33fcc736e396de8768eb2871106aa34e65dce
EngPeterMaged <eng.peter.maged@gmail.com>
2021-04-27 13:08:46 +0200

    customer_agrement_dashboard

--- erpnext/operations/doctype/custody_movement/custody_movement.js
+++ erpnext/operations/doctype/custody_movement/custody_movement.js
@@ -12,6 +12,22 @@ frappe.ui.form.on('Custody Movement', {
         frm.events.set_fields_query(frm)
 
 	},
+    to_customer_agreement: function(frm) {
+
+        frm.events.set_fields_status(frm)
+        frm.events.set_fields_query(frm)
+
+	},
+    from_customer_agreement: function(frm) {
+        frm.events.set_fields_status(frm)
+        frm.events.set_fields_query(frm)
+
+	},
+    is_custody: function(frm) {
+        frm.events.set_fields_status(frm)
+        frm.events.set_fields_query(frm)
+
+	},
     set_fields_status:function (frm) {
         if (frm.doc.type == 'Send'){
             frm.set_df_property('to_customer_agreement','hidden',0)
@@ -33,6 +49,9 @@ frappe.ui.form.on('Custody Movement', {
             frm.set_df_property('source_warehouse','hidden',1)
             frm.set_df_property('source_warehouse','reqd',0)
 
+            frm.set_df_property('custody_warehouse','hidden',1)
+            frm.set_df_property('custody_warehouse','reqd',0)
+
 
         }
         if (frm.doc.type == 'Transfer'){
@@ -55,7 +74,8 @@ frappe.ui.form.on('Custody Movement', {
             frm.set_df_property('source_warehouse','hidden',0)
             frm.set_df_property('source_warehouse','reqd',1)
 
-
+            frm.set_df_property('custody_warehouse','hidden',1)
+            frm.set_df_property('custody_warehouse','reqd',0)
         }
         if (frm.doc.type == 'Return' || frm.doc.type == 'Deliver'){
             frm.set_df_property('to_customer_agreement','hidden',1)
@@ -77,8 +97,39 @@ frappe.ui.form.on('Custody Movement', {
             frm.set_df_property('source_warehouse','hidden',0)
             frm.set_df_property('source_warehouse','reqd',1)
 
+            frm.set_df_property('custody_warehouse','hidden',1)
+            frm.set_df_property('custody_warehouse','reqd',0)
 
         }
+        if ( frm.doc.type == 'Receipt from Customer'){
+            frm.set_df_property('to_customer_agreement','hidden',1)
+            frm.set_df_property('to_customer_agreement','reqd',0)
+
+            frm.set_df_property('to_employee','hidden',1)
+            frm.set_df_property('to_employee','reqd',0)
+
+
+            frm.set_df_property('target_warehouse','hidden',1)
+            frm.set_df_property('target_warehouse','reqd',0)
+
+
+
+
+            frm.set_df_property('source_warehouse','hidden',1)
+            frm.set_df_property('source_warehouse','reqd',0)
+
+            frm.set_df_property('custody_warehouse','hidden',0)
+            frm.set_df_property('custody_warehouse','reqd',1)
+
+             frm.set_df_property('from_customer_agreement','hidden',0)
+            frm.set_df_property('from_customer_agreement','reqd',1)
+
+             frm.set_df_property('from_employee','hidden',!cur_frm.doc.is_custody)
+            frm.set_df_property('from_employee','reqd',cur_frm.doc.is_custody)
+
+
+        }
+
     },
     set_fields_query:function(frm){
 
@@ -98,12 +149,9 @@ frappe.ui.form.on('Custody Movement', {
                 }
             };
 
-	    var to_custody_warehouse = 0
-        var from_custody_warehouse = 0
 
 	     if (frm.doc.type == 'Send' ){
-	         to_custody_warehouse = 1
-             from_custody_warehouse = 0
+
             frm.fields_dict['items'].grid.get_field('item_code').get_query = function(doc, cdt, cdn){
 
                 return {
@@ -114,8 +162,7 @@ frappe.ui.form.on('Custody Movement', {
 	     }
         if (frm.doc.type == 'Transfer'){
 
-            to_custody_warehouse = 1
-             from_custody_warehouse = 1
+
 
             frm.fields_dict['items'].grid.get_field('item_code').get_query = function(doc, cdt, cdn){
 
@@ -130,9 +177,8 @@ frappe.ui.form.on('Custody Movement', {
 
 
         }
-        if (frm.doc.type == 'Return' || frm.doc.type == 'Deliver'){
-              to_custody_warehouse = 0
-             from_custody_warehouse = 1
+        if (frm.doc.type == 'Return' || frm.doc.type == 'Deliver' || frm.doc.type == 'Receipt from Customer' ){
+
             frm.fields_dict['items'].grid.get_field('item_code').get_query = function(doc, cdt, cdn){
                 return {
                     query:"dynamicerp.dynamic_erp.doctype.stock_entry.stock_entry.get_items_query",
@@ -145,20 +191,19 @@ frappe.ui.form.on('Custody Movement', {
         }
 
 
-          frm.set_query("target_warehouse" , function(frm){
-                     return {
-                         filters :{
-                            custody_warehouse:to_custody_warehouse
-                         }
+
+
+        frm.clear_table("items")
+        refresh_field('items')
+
+        frm.set_query("custody_warehouse" , function(frm){
+                 return {
+                     filters :{
+                        customer_custody_warehouse:1
                      }
-                 })
-                 frm.set_query("source_warehouse" , function(frm){
-                     return {
-                         filters :{
-                            custody_warehouse:from_custody_warehouse
-                         }
-                     }
-                 })
+                 }
+             })
+
 
 
 
--- erpnext/operations/doctype/custody_movement/custody_movement.py
+++ erpnext/operations/doctype/custody_movement/custody_movement.py
@@ -38,26 +38,37 @@ class CustodyMovement(Document):
 			'Deliver': 'Material Issue',
 			'Transfer': 'Custody Transfer',
 			'Send': 'Custody Send',
+			'Receipt from Customer': 'Material Receipt',
 			'Return': 'Custody Return'
 		}
 		# IN Case Transfer
 		source_warehouse = self.source_warehouse
 		target_warehouse = self.target_warehouse
-
+		stock_entry = frappe.new_doc("Stock Entry")
+		stock_entry.is_custody = 1
 		# IN Case Send
+		agreement = None
 		if self.type == 'Send':
 			source_warehouse , target_warehouse = frappe.db.get_value('Customer Agrement',self.to_customer_agreement , ['sorce_warehouse','warehouse'])
 
 		# IN Case Return  And Deliver
 		elif  self.type in ['Return','Deliver'] :
 			target_warehouse , source_warehouse   = frappe.db.get_value('Customer Agrement',self.from_customer_agreement , ['sorce_warehouse','warehouse'])
+		elif  self.type in ['Receipt from Customer'] :
+			target_warehouse    = self.custody_warehouse
+			source_warehouse    =  self.custody_warehouse
+			agreement = frappe.get_doc('Customer Agrement', self.from_customer_agreement)
+			# validiate items deliver > 0
+			for i in self.items :
 
-		frappe.msgprint(source_warehouse)
-		frappe.msgprint(target_warehouse)
+				delivered_qty = sum([item.delivered_qty for item in agreement.tools if item.item_code == i.item_code and item.uom  == i.uom and item.delivered_qty > 0  ])
+				if delivered_qty < i.qty :
+					frappe.throw(_("Item {} in row {} has been delivered only {} {}".format(i.item_code , i.idx , delivered_qty , i.uom )))
+			stock_entry.is_custody = self.is_custody
 
 
 
-		stock_entry = frappe.new_doc("Stock Entry")
+
 		stock_entry.stock_entry_type = types [self.type]
 		stock_entry.from_warehouse = source_warehouse
 		stock_entry.to_warehouse = target_warehouse
@@ -69,7 +80,7 @@ class CustodyMovement(Document):
 		stock_entry.from_employee = self.from_employee
 		stock_entry.employee = self.to_employee
 
-		stock_entry.is_custody = 1
+
 		for item in self.items:
 				if (item.qty) > 0:  # and not item.delivered and item.delivered_qty < item.qty:
 					se_child = stock_entry.append('items')
@@ -90,6 +101,19 @@ class CustodyMovement(Document):
 		stock_entry.submit()
 		self.stock_entry = stock_entry.name
 		self.save()
+		if self.type in ['Receipt from Customer']:
+			for i in self.items:
+				delivered = [item for item in agreement.tools if item.item_code == i.item_code and item.uom == i.uom and item.delivered_qty > 0]
+				qty = i.qty
+				for item in	delivered :
+					if qty > 0:
+						if item.delivered_qty >= qty :
+							frappe.db.set_value("Customer Agrement Tools",item.name,'delivered_qty',item.delivered_qty-qty)
+							qty = 0
+						else :
+							frappe.db.set_value("Customer Agrement Tools",item.name,'delivered_qty',0)
+							qty -= item.delivered_qty
+
 
 		l = """ <b><a href="#Form/{0}/{1}">{1}</a></b>""".format(stock_entry.doctype, stock_entry.name)
 		msg = _("A {} {} is Created for Company {}").format(stock_entry.doctype, l, stock_entry.company)
